0e48110a607870ab8ea33f6f559fd87cf9648048
David García Alonso <g.a.david@gmail.com>
2017-10-31 15:22:39 +0100

    Refactor. p. 504

--- bloggy_project/blog/views.py
+++ bloggy_project/blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
 
-# Create your views here.
+from blog.models import Post
+
+
+def index(request):
+    latest_posts = Post.objects.all().order_by('-created_at')
+    t = loader.get_template('blog/index.html')
+    c = {'latest_posts': latest_posts, }
+    return HttpResponse(t.render(c))
