a5a640112d4fb21cf6868692561b82165b85d871
David García Alonso <g.a.david@gmail.com>
2017-10-31 13:29:00 +0100

    Templates and Views. p. 502

--- bloggy_project/blog/admin.py
+++ bloggy_project/blog/admin.py
@@ -1,3 +1,13 @@
+# blog/admin.py
+
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+
+
+admin.site.register(Post, PostAdmin)
